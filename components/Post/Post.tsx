74b411348c0a6a663308ccfc03f094e07c538274
A.J. Hawkey <aaronhawkey48@gmail.com>
2020-05-09 18:13:49 -0500

    missing work

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -9,11 +9,13 @@ const Post  = (props) => {
             <div className={styles.post}>
                 <div className={styles.postHeader}>
                     <Avatar className={styles.avatar}>AH</Avatar>
-                    <div>
-                        <p>Aaron Hawkey</p>
-                    </div>
-                    <div>
-                        <p>Maralago</p>
+                    <div className={styles.headerText}>
+                        <div className={styles.headerTextName}>
+
+                        </div>
+                        <div className={styles.headerTextIsland}>
+
+                        </div>
                     </div>
                     <div className={styles.postOptions}>
                     <IconButton><MoreVertIcon /></IconButton>
