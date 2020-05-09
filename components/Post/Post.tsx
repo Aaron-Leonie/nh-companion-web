273d89ea96af7cbdc99fa6e73b83b67a73d93fb9
A.J. Hawkey <aaronhawkey48@gmail.com>
2020-05-09 18:27:18 -0500

    fixed error

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -10,12 +10,13 @@ const Post  = (props) => {
                 <div className={styles.postHeader}>
                     <Avatar className={styles.avatar}>AH</Avatar>
                     <div className={styles.headerText}>
-                        <div className={styles.headerTextName}>
+                        {/* <div className={styles.headerTextName}>
 
                         </div>
                         <div className={styles.headerTextIsland}>
 
-                        </div>
+                        </div> */}
+                        Hello
                     </div>
                     <div className={styles.postOptions}>
                     <IconButton><MoreVertIcon /></IconButton>
